69e33006f0b5da4772e6bcc7c2f57e98ca13a3c0
Bryan M <janbryanmartirez@gmail.com>
2018-09-21 08:32:11 +0800

    first commit

---
+++ src/components/featured/Timer.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+class Timer extends Component {
+
+    state = {
+
+    }
+
+    render() {
+        return (
+            <div className='countdown_wrapper'>
+                <div className='countdown_top'>
+                    Event Starts In
+                </div>
+                <div className='countdown_bottom'>
+                    Aw
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Timer;
